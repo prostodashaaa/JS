3b797f5fea8abff3450cc4dfea98b707b48d1cf5
Darya <kozyreva2007sv@gmail.com>
2024-08-11 18:19:35 +0300

    fix

--- JavaScript_Advanced/1-map/map.js
+++ JavaScript_Advanced/1-map/map.js
@@ -2,7 +2,9 @@ const array = [
   { id: 1, name: "Вася" },
   { id: 2, name: "Петя" },
   { id: 1, name: "Вася" },
+  { id: 6, name: "Вася" },
+  { id: 6, name: "Вася" },
 ];
 
-const arraySet = Array.from(new Set(array.map(JSON.stringify))).map(JSON.parse);
+const arraySet = new Set(array.map(obj => obj.id).map(id => array.find(item => item.id === id)));
 console.log(arraySet);
